07288caab1939fe9efddf54b6b9690e0f3c36d9d
Fernando Aranzabe <faranzabe@gmail.com>
2024-04-02 19:06:53 +0200

    Enigma1_tipos_datos

---
+++ Assets/Scripts/EncenderLinterna.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncenderLinterna : MonoBehaviour
+{
+    public GameObject luz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void encenderLinterna()
+    {
+        luz.SetActive(true);
+    }
+
+    public void apagarLinterna()
+    {
+        luz.SetActive(false);
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log(collision.gameObject.tag);
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+
+    //    }
+    //    luz.SetActive(true);
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    luz.SetActive(false);
+    //}
+}
