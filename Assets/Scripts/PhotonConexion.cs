17b857bbeead03731b20dfb39831fb4e54a7eae7
Jose Alberto Garcia <jagarcias1978@gmail.com>
2024-04-24 09:48:53 +0200

    Script conexión Photon

---
+++ Assets/Scripts/PhotonConexion.cs
@@ -0,0 +1,29 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonConexion : MonoBehaviourPunCallbacks
+{
+    //public PhotonView playerPrefab;
+    //public Transform spawnPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Usará las settings de nuestro servidor.
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conectados!!");
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
