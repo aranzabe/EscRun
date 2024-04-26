bf02a34bcf1c8a3c603833f40c5dd33d6cf63b52
Jose Alberto Garcia <jagarcias1978@gmail.com>
2024-04-26 10:53:24 +0200

    Photon con jugador "esfera"

--- Assets/Scripts/PhotonConexion.cs
+++ Assets/Scripts/PhotonConexion.cs
@@ -1,12 +1,13 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PhotonConexion : MonoBehaviourPunCallbacks
 {
-    public PhotonView playerPrefab;
     public Transform spawnPoint;
+    private GameObject spawnedPlayerPrefab;
 
 
     // Start is called before the first frame update
@@ -18,12 +19,24 @@ public class PhotonConexion : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         //Debug.Log("Conectados!!");
-        PhotonNetwork.JoinRandomOrCreateRoom();
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("EscRun1",roomOptions,TypedLobby.Default);
     }
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        base.OnJoinedRoom();
+        //"Network Player" se debe llamar igual que el Empty Object que hay en la jerarquía
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", spawnPoint.position, spawnPoint.rotation);
     }
 
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
 }
