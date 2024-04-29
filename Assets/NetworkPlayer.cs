29f03e604a4a80accf69c21956ff47c2de85ad71
Jose Alberto Garcia <jagarcias1978@gmail.com>
2024-04-29 12:15:44 +0200

    Script NetworkPlayer para evitar vernos a nosotros y ver a los demás

---
+++ Assets/NetworkPlayer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+    private PhotonView photonView;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MapPosition(head, XRNode.Head);
+        MapPosition(leftHand, XRNode.LeftHand);
+        MapPosition(rightHand, XRNode.RightHand);
+    }
+
+    void MapPosition(Transform target, XRNode node)
+    {
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(UnityEngine.XR.CommonUsages.devicePosition, out Vector3 position);
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceRotation, out Quaternion rotation);
+
+        target.position = position;
+        target.rotation = rotation;
+    }
+}
